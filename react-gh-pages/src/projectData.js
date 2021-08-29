dab8a8ed3ca8364b402389bead7df4d146ff8aed
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-08-29 17:36:26 -0400

    Updated package.json with npm packages

---
+++ react-gh-pages/src/projectData.js
@@ -0,0 +1,51 @@
+import travlr from './images/travlr.png';
+import recipe from './images/recipe-cookbook.png';
+import atmos from './images/atmos.png';
+import planner from './images/work-planner.png';
+import dash from './images/weather.dash.png';
+import password from './images/password-generator.png';
+
+export const projects = [
+    {
+        title: 'Travlr',
+        subtitle: 'React, React-Bootstrap, JavaScript, HTML',
+        github: 'https://github.com/vyncent-t/travlr-project',
+        image: travlr,
+        link:'https://travlr-proj.herokuapp.com/'
+    },
+    {
+        title: 'My Recipe Cookbook',
+        subtitle: 'JavaScript, Handlebars, CSS, HTML',
+        github: 'https://github.com/JuneXll/Recipe-Project',
+        image: recipe,
+        link:'https://cryptic-dusk-68462.herokuapp.com/'
+    },
+    {
+        title: 'Atmos',
+        subtitle: 'JavaScript, jQuery, Bulma',
+        github: 'https://github.com/vyncent-t/atmosphere-project',
+        image: atmos,
+        link: 'https://vyncent-t.github.io/atmosphere-project/'
+    },
+    {
+        title: 'Work Planner',
+        subtitle: 'HTML, CSS, JavaScript',
+        github: 'https://github.com/JuneXll/day-planner',
+        image: planner,
+        link: 'https://junexll.github.io/day-planner/'
+    },
+    {
+        title: 'Weather Dashboard',
+        subtitle: 'HTML, CSS, Bootstrap',
+        github: 'https://github.com/JuneXll/Weather-Dashboard',
+        image: dash,
+        link: 'https://junexll.github.io/Weather-Dashboard/'
+    },
+    {
+        title: 'Password-Generator',
+        subtitle: 'JavaScript',
+        github: 'https://github.com/JuneXll/password-generator',
+        image: password,
+        link: 'https://junexll.github.io/password-generator/'
+    }
+]
