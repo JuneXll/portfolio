3af98d79dbeb8faf524fe872b16dfdc90851494b
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-08-29 18:34:01 -0400

    saving work on nav

--- react-gh-pages/src/App.js
+++ react-gh-pages/src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import NavBar from './components/NavBar';
+import Home from './components/pages/Home';
+import Experience from './components/pages/Experience';
+import Projects from './components/pages/Projects';
+import Contact from './components/pages/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <>
+        <NavBar/>
+        <Switch>
+          <Container>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/Experience' component={Experience}/>
+            <Route exact path='/Projects' component={Projects}/>
+            <Route exact path='/Contact' component={Contact}/>
+          </Container>
+          <Route render={()=><h1 className='display-2'>Oops! Wrong Page!</h1>}/>
+        </Switch>
+        <Footer/>
+      </>
+    </Router>
   );
 }
 
---
+++ react-gh-pages/src/components/widget/ProjectCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProjectCard = ()=>{
+    return(
+        <h1>project card</h1>
+    )
+}
+
+export default ProjectCard;
