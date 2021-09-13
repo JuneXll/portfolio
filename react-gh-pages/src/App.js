2741e9af39505f7a4f43af91ee07fe4d0b8059ed
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-09-13 10:32:17 -0400

    fixed parth for home

--- react-gh-pages/src/App.js
+++ react-gh-pages/src/App.js
@@ -24,7 +24,7 @@ function App() {
         <Switch>
           <Container fluid className='col overflow-auto p-0'>
             <Route exact path='/' component={Home}/>
-            <Route exact path='/react-gh-pages' component={Home}/>
+            <Route exact path='/portfolio' component={Home}/>
             <Route exact path='/tech' component={Tech}/>
             <Route exact path='/projects' component={Projects}/>
             <Route exact path='/contact' component={Contact}/>
